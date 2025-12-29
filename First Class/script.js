3fa4ef5b81169a78157e611a86aa7b770250532d
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2025-12-29 19:18:31 +0500

    Perform different math function,Operator and aslo solve practical question on it

--- First Class/script.js
+++ First Class/script.js
@@ -80,4 +80,126 @@
 
 // or abhi humber division hain or wo float main number ko retur karthe hain or eik baath or hain or ye qouetient ko return kartha hain diher
 
-console.log(7 / 2); // pr ye 3... something ko return kar dein or iss say ye bhi zahir hota hain keh 7 kitni dafa divide ho chuka hain
+// console.log(7 / 2); // pr ye 3... something ko return kar dein or iss say ye bhi zahir hota hain keh 7 kitni dafa divide ho chuka hain
+
+// ****************************************************************************************************************************************
+
+// uniary operator
+// Unary operation wo hoti hai jisme sirf ek operand (value) par operation perform hota hai.
+
+// Example :1
+// let a = 10;
+// let b = a++;
+// console.log("The value of a now is ", a); // 11
+// console.log("The value of b is now ", b); // 10
+
+// Example : 2
+
+// let a = ++"10"
+// console.log("The value of a is",a)  //Ye error is liye aata hai kyon ke ++ operator string par direct apply nahi hota, pehle variable chahiye hota hai, is liye "10" par increment allowed nahi.
+
+// Example : 3
+// let a = ++10;
+// console.log("the value of a ",a)  // Ye is liye kaam nahi karta kyon ke ++ operator sirf variables par lagta hai, direct constant/value (10) par lagana JavaScript main allowed nahi hota.
+
+// Question 1:
+
+// let a = 11;
+// let b = 22;
+// let c = a + b + a++ + b++ + ++a + ++b;
+// console.log("a is", a);
+// console.log("b is", b);
+// console.log("C is", c);
+
+// ****************************************************************************************************************************************
+
+// Math.round()
+// Rounds a number to the nearest whole number
+console.log(Math.round(4.6)); // 5
+console.log(Math.round(4.4)); // 4
+
+// Math.ceil()
+// Rounds a number UP to the next whole number
+
+// console.log(Math.ceil(4.1)); // 5
+// console.log(Math.ceil(4.9)); // 5
+
+// Math.floor()
+// Rounds a number DOWN to the previous whole number
+// Math.floor(4.9); // 4
+// Math.floor(4.1); // 4
+
+// Math.trunc()
+// Removes the decimal part (no rounding)
+
+//console.log(Math.trunc(4.9)); // 4
+//console.log(Math.trunc(4.1)); // 4
+
+// Math.pow()
+// Raises a number to a power
+// console.log(Math.pow(2, 3)); // 8  (2 × 2 × 2)
+
+// Math.sqrt()
+// Gives the square root of a number
+// console.log(Math.sqrt(16)); // 4
+
+// Math.cbrt()
+// Gives the cube root of a number
+// console.log(Math.cbrt(27)); // 3
+
+// Math.abs()
+// Returns the positive value (removes minus sign)
+// console.log(Math.max(3, 7, 2)); //7
+
+// Math.min()
+
+// Finds the smallest number
+// console.log(Math.min(3, 7, 2)); // 2
+
+// Math.random()
+// Gives a random number between 0 and 1
+
+// console.log(Math.trunc(Math.random() * 9000 + 1000));
+
+// toFixed()
+
+// Limits decimal places (returns a string)
+
+// let num = 3.14159;
+// num.toFixed(2); // "3.14"
+
+// ****************************************************************************************************************************************
+
+// And the last we have Four Questions
+
+// ****************************************************************************************************************************************
+
+// Calculate area and perimeter of rectangel
+// let l = 2;
+// let w = 20;
+// console.log("The area of perimeter of reactangle", 2 * (l * w));
+
+// *******************************************************************
+// //  Generate OTP of 4 digits
+// let otp = Math.random() * 9000 + 1000;
+// console.log("Four Digit OTP", Math.trunc(otp));
+
+// *******************************************************************
+
+// Area of triangle by heron's formula
+
+// let a = 6;
+// let b = 4;
+// let c = 3;
+
+// let s = (a + b + c) / 2;
+// console.log("S", s);
+
+// let result = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+// console.log("Area of trianlge by heron's formula is ", Math.trunc(result));
+
+// *******************************************************************
+
+// Circumference of circle
+let r = 5;
+console.log("The circuference of circle is ", Math.trunc(2 * Math.PI * r));
