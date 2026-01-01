f54e4f2f39dcafb1525a8351afb81fc4a88986e9
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2026-01-01 05:33:22 +0500

    create separate file for dsa questions

---
+++ First Class/questions.js
@@ -0,0 +1,36 @@
+// ****************************************************************************************************************************************
+
+// And the last we have Four Questions
+
+// ****************************************************************************************************************************************
+
+// Calculate area and perimeter of rectangle
+// let l = 2;
+// let w = 20;
+// console.log("The area of perimeter of reactangle", 2 * (l * w));
+
+// *******************************************************************
+
+// Generate OTP of 4 digits
+// let otp = Math.random() * 9000 + 1000;
+// console.log("Four Digit OTP", Math.trunc(otp));
+
+// *******************************************************************
+
+// Find Area of triangle by heron's formula
+
+// let a = 6;
+// let b = 4;
+// let c = 3;
+
+// let s = (a + b + c) / 2;
+// console.log("S", s);
+
+// let result = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+// console.log("Area of trianlge by heron's formula is ", Math.trunc(result));
+
+// *******************************************************************
+
+// Find Circumference of circle
+let r = 5;
+console.log("The circuference of circle is ", Math.trunc(2 * Math.PI * r));
--- First Class/script.js
+++ First Class/script.js
@@ -168,38 +168,4 @@ console.log(Math.round(4.4)); // 4
 // let num = 3.14159;
 // num.toFixed(2); // "3.14"
 
-// ****************************************************************************************************************************************
 
-// And the last we have Four Questions
-
-// ****************************************************************************************************************************************
-
-// Calculate area and perimeter of rectangel
-// let l = 2;
-// let w = 20;
-// console.log("The area of perimeter of reactangle", 2 * (l * w));
-
-// *******************************************************************
-// //  Generate OTP of 4 digits
-// let otp = Math.random() * 9000 + 1000;
-// console.log("Four Digit OTP", Math.trunc(otp));
-
-// *******************************************************************
-
-// Area of triangle by heron's formula
-
-// let a = 6;
-// let b = 4;
-// let c = 3;
-
-// let s = (a + b + c) / 2;
-// console.log("S", s);
-
-// let result = Math.sqrt(s * (s - a) * (s - b) * (s - c));
-// console.log("Area of trianlge by heron's formula is ", Math.trunc(result));
-
-// *******************************************************************
-
-// Circumference of circle
-let r = 5;
-console.log("The circuference of circle is ", Math.trunc(2 * Math.PI * r));
