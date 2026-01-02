8577c0d3dea4bbd37046eab7d21b619ecc02bd5b
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2026-01-02 20:10:55 +0500

    create sperate file for eact ones

---
+++ Third Class/factorials.js
@@ -0,0 +1,20 @@
+// Questions 2: Find n of natural number of factorials
+
+// let input = prompt("Enter number here");
+
+// if (input === null) {
+//   console.log("Prompt cancelled....");
+// } else {
+//   let n = Number(input);
+//   if (isNaN(n)) {
+//     console.log("Wrong Input");
+//   } else if (n > 0) {
+//     let fact = 1;
+//     for (let i = 1; i <= n; i++) {
+//       fact = fact * i;
+//     }
+//     console.log("Factorial is", fact);
+//   } else {
+//     console.log("Input should be positive and not zero");
+//   }
+// }
